74e43fd0435847c73863590e2eeb865f99a1ca06
TZebra <100721290+Tim-Zebra@users.noreply.github.com>
2022-05-02 09:58:42 -0500

    added FOREIGN KEY REFERENCing

--- db/schema.sql
+++ db/schema.sql
@@ -12,7 +12,9 @@ CREATE TABLE role (
   id INT PRIMARY KEY NOT NULL,
   title VARCHAR(30) NOT NULL,
   salary DECIMAL NOT NULL,
-  department_id INT NOT NULL
+  department_id INT NOT NULL,
+  FOREIGN KEY (department_id)
+  REFERENCES department(id)
 );
 
 CREATE TABLE employee (
@@ -20,5 +22,9 @@ CREATE TABLE employee (
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
   role_id INT NOT NULL,
-  manager_id INT
+  FOREIGN KEY (role_id)
+  REFERENCES role(id),
+  manager_id INT,
+  FOREIGN KEY (manager_id)
+  REFERENCES employee(id)
 );
