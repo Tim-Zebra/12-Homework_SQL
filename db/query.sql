3bb13338d4196995a9a8e14bccc44ab2a9fb41e9
TZebra <100721290+Tim-Zebra@users.noreply.github.com>
2022-05-02 17:21:03 -0500

    Able to join between employee, role, and dept table. Will attempt to to link with manager for next commit

--- db/query.sql
+++ db/query.sql
@@ -9,6 +9,7 @@ JOIN department ON role.department_id = department.id;
 
 -- View Employees: employee ids, first names, last names, 
 -- job titles, departments, salaries, and managers that the employees report to
-SELECT employee.id, employee.first_name, employee.last_name, role.title, role.salary
+SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary
 FROM employee
-JOIN role ON employee.role_id = role.id;
+INNER JOIN role ON employee.role_id = role.id 
+INNER JOIN department ON role.department_id = department.id;
